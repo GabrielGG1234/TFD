f3cba138dce8826fdfc405dbf9b93f4b6b2dd55e
Gabriel Antunes <xablauia74@gmail.com>
2022-11-30 18:28:49 -0300

    Update iniciar.sh

--- provisions/iniciar.sh
+++ provisions/iniciar.sh
@@ -1,12 +1,7 @@
-sudo apt update
-sudo apt upgrade
+
 sudo apt install virtualbox -y
 wget https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb
 sudo apt install ./vagrant_2.2.19_x86_64.deb
-
-
-
-
 
 cd ../front
 vagrant up --provision
